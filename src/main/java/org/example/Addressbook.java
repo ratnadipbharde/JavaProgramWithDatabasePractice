9093b5311705e1366eb86f0afe66eada3ec6ea51
ratnadipbharde <ratnadipbharde@gmail.com>
2022-12-25 04:02:00 +0530

    added add contact to database and show data in database

--- src/main/java/org/example/Addressbook.java
+++ src/main/java/org/example/Addressbook.java
@@ -1,7 +1,5 @@
 package org.example;
-
-import com.sun.org.apache.xpath.internal.objects.XNumber;
-
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,12 +7,74 @@ import java.util.Scanner;
 public class Addressbook {
     List<Contact> addressbookList =new ArrayList<>();
     Scanner sc=new Scanner(System.in);
+    String classname="com.mysql.cj.jdbc.Driver";
+    String url="jdbc:mysql://localhost:3306/paddressbook?useSSL=False";
+    String username="root";
+    String password="root";
+
+    public Connection getCon()
+    {
+        Connection con = null;
+        try {
+            Class.forName(classname);
+            con= DriverManager.getConnection(url, username, password);
+            if(con!=null)
+            {
+                System.out.println("connected..");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return con;
+    }
+
+    public void showAllContactFromDatabase() {
+        Connection con = getCon();
+        try {
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery("select * from addressbook");
+            System.out.println("---------Records are---------");
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+            System.out.println("-----------------------------");
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public void addAllContactToDatabase(){
+        addressbookList.forEach(contact -> {
+            Connection con=getCon();
+            try {
+                String sql="insert into addressbook(firstname,lastname,city) values (?,?,?)";
+                PreparedStatement ps=con.prepareStatement(sql);
+                ps.setString(1, contact.getFirstName());
+                ps.setString(2, contact.getLastName());
+                ps.setString(3, contact.getCity());
+                int i=ps.executeUpdate();
+                if(i>0)
+                {
+                    System.out.println(i+"row affected");
+                }
+                con.close();
+                System.out.println("\nSave in Database successfully..... \n");
+            }
+            catch(Exception e)
+            {
+                System.out.println(e);
+            }
+        });
+    }
 
     public void addContactInAddressbook(){
         System.out.print("how many contact you want to save :");
         int number= sc.nextInt();
         for (int i = 0; i < number; i++) {
-            System.out.println("insert contact : "+i);
+            System.out.println("insert contact : "+(i+1));
             System.out.println("First Name : ");
             String firstName=sc.next();
             System.out.println("Last Name : ");
@@ -23,7 +83,8 @@ public class Addressbook {
             String city=sc.next();
             Contact contact = new Contact(firstName,lastName,city);
             addressbookList.add(contact);
+            System.out.println("---------------------\n");
         }
-
+        System.out.println("\nadded successfully..... \n");
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,32 @@
 package org.example;
+
 import java.util.Scanner;
 
 public class Main {
-    static final int ADDCONTACTSINADDRESSBOOK =1;
-    static final int EXIT =0;
+    static final int ADDCONTACTSINADDRESSBOOK = 1;
+    static final int ADDALLCONTACTSTODATABASE = 2;
+    static final int ADDALLCONTACTSFROMDATABASE = 3;
+    static final int EXIT = 0;
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Addressbook");
-        System.out.println("1.Add contacts in ContactList");
         Scanner sc = new Scanner(System.in);
-        Addressbook addressbook=new Addressbook();
+        Addressbook addressbook = new Addressbook();
         while (true) {
+            System.out.println("\n\t\t--------- Welcome to Addressbook ---------\n");
+            System.out.print("0. Exit.\n1. Add contacts in ContactList.\n2. Save all contacts to database.\n3. Show all contacts from database.\n");
+            System.out.print("\nInput your Choice : ");
             switch (sc.nextInt()) {
                 case ADDCONTACTSINADDRESSBOOK:
-                  addressbook.addContactInAddressbook();
-                  break;
+                    addressbook.addContactInAddressbook();
+                    break;
+                case ADDALLCONTACTSTODATABASE:
+                    addressbook.addAllContactToDatabase();
+                    break;
+                case ADDALLCONTACTSFROMDATABASE:
+                    addressbook.showAllContactFromDatabase();
+                    break;
                 case EXIT:
-                  return;
+                    return;
             }
         }
     }
